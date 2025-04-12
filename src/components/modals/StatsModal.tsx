c9d3653eee948be1cac9fdea1262c83ce6399a9d
BHunt <benjaminhunt93@gmail.com>
2025-04-12 14:33:01 -0500

    dictionary look up button

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -53,8 +53,8 @@ export const StatsModal = ({
       </h4>
       <Histogram gameStats={gameStats} />
       {(isGameLost || isGameWon) && (
-        <a target="_blank" rel="noopener noreferrer" href={hrefLink}>View Solution in Dictionary</a>
         <div className="mt-5 sm:mt-6 columns-2">
+          {/*<a target="_blank" rel="noopener noreferrer" href={hrefLink}>View Solution in Dictionary</a>*/}
           <div>
             <h5>{t('newWordCountdown')}</h5>
             <Countdown
@@ -63,6 +63,15 @@ export const StatsModal = ({
               daysInHours={true}
             />
           </div>
+          <button
+            type="button"
+            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            onClick={() => {
+              window.location.href = hrefLink;
+            }}
+          >
+            {t('look it up')}
+          </button>
           <button
             type="button"
             className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
